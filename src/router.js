6cccbc918716e1d1505189ef665feda87c475e4a
王栋卿 <bestriven666@outlook.com>
2020-03-17 20:56:18 +0800

    完成了登录功能

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login'
+import Home from './components/Home'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+
+// 挂载路由导航守卫
+// 只要登录成功之后，才能访问后面的网页，如果没有登录，自动跳转到login
+// router.beforeEach((to, from, next) => {
+//   // to 将要访问的路径
+//   // from 代表从哪个路径跳转而来
+//   // next 是一个函数，表示放行
+//   //     next()  放行    next('/login')  强制跳转
+//
+//   if (to.path === '/login') return next()
+//   // 获取token
+//   const tokenStr = window.sessionStorage.getItem('token')
+//   if (!tokenStr) return next('/login')
+//   next()
+// })
+
+export default router
